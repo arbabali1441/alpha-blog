65c678f3f134cc042ae2894c28ba64e658d8e56b
arbabali1441 <arbabali1441@gmail.com>
2020-12-20 13:16:41 +0500

    completed category model and unit test integration video # 129

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201130143603) do
+ActiveRecord::Schema.define(version: 20201202112010) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20201130143603) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
