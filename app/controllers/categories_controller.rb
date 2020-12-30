871dbd72130534a2f4a49adc88021cb171561aeb
arbabali1441 <arbabali1441@gmail.com>
2020-12-30 13:54:42 +0500

    Create Category test, model 10:53

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,10 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
 
   end
 
--- app/controllers/categories_controller_test.rb
+++ app/controllers/categories_controller_test.rb
@@ -20,3 +20,4 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
 end
+  
